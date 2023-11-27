c633333fe82ebf050fcd65949c253c50daa650d5
Mica Swyers <5359472+micaswyers@users.noreply.github.com>
2023-11-27 11:22:38 +0000

    Create initial GuessInput component and add to Game

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function GuessInput() {
+  return <form>
+    <label>Enter your guess here</label>
+    <input type="text"></input>
+  </form>;
+}
+
+export default GuessInput;
