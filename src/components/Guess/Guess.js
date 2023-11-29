bc8562ab9307533ab52c58dcd42166ed5217ef27
Mica Swyers <5359472+micaswyers@users.noreply.github.com>
2023-11-29 20:30:34 +0000

    Fix based on Josh's solution

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,16 +2,23 @@ import React from "react";
 import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+  return (<span className={className}>{letter}</span>);
+}
+
 function Guess({ answer, word }) {
   const checkedWordObj = checkGuess(word, answer);
 
   return (
-    <p className="guess" key={crypto.randomUUID()}>
+    <p className="guess">
       {range(5).map((idx) => {
         return (
-          <span className={`cell ${checkedWordObj ? checkedWordObj[idx].status: ""}`} key={crypto.randomUUID()}>
-            {word ? checkedWordObj[idx].letter : undefined}
-          </span>
+          <Cell
+            key={crypto.randomUUID()}
+            letter={checkedWordObj ? checkedWordObj[idx].letter : undefined}
+            status={checkedWordObj ? checkedWordObj[idx].status : undefined}
+          />
         );
       })}
     </p>
