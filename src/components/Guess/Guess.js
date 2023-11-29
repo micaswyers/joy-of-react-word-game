cce030da9998b77137cb2b2e0f9bf92ac405b34f
Mica Swyers <5359472+micaswyers@users.noreply.github.com>
2023-11-29 20:11:34 +0000

    Show colors based on status of letters

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ word }) {
+function Guess({ answer, word }) {
+  const checkedWordObj = checkGuess(word, answer);
+
   return (
     <p className="guess" key={crypto.randomUUID()}>
       {range(5).map((idx) => {
         return (
-          <span className="cell" key={crypto.randomUUID()}>
-            {word[idx]}
+          <span className={`cell ${checkedWordObj ? checkedWordObj[idx].status: ""}`} key={crypto.randomUUID()}>
+            {word ? checkedWordObj[idx].letter : undefined}
           </span>
         );
       })}
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,12 +3,12 @@ import Guess from "../Guess";
 import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
-function GuessResults({ guesses }) {
+function GuessResults({ answer, guesses }) {
   return (
     <div className="guess-results">
       {
         range(0,NUM_OF_GUESSES_ALLOWED).map((num) => {
-          return <Guess word={guesses[num] ?? ""} key={crypto.randomUUID()}></Guess>
+          return <Guess answer={answer} word={guesses[num]} key={crypto.randomUUID()}></Guess>
         })
       }
     </div>
